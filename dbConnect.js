5f8eb6acecac9b5b0bc9ad1abf1ded68134cb723
Redoy hossen emon <redoy.hossen.emon@gmail.com>
2025-09-21 12:28:58 +0600

    Update dbConnect.js

--- dbConnect.js
+++ dbConnect.js
@@ -1,8 +1,9 @@
 
-mongoose.connect('mongodb://127.0.0.1:27017/testDB')
+mongoose.connect(DB_URL)
   .then(() => {
     console.log("server connected..")
   }).catch((err) => {
     console.log("erro: ", err);
+
 
   })
